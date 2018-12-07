25796983b2df0847e98f164b72d2f350aaa725ca
Alexandra <aalarenas1800@gmail.com>
2018-12-07 11:59:28 -0600

    contactos hecho

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,7 +14,7 @@ info= InfoPage;
   contactos = [
     {nombre: " Alexandra Arenas ", tel: " +52 1 (33) 1835 6599",correo: " aalarenas1800@gmail.com", fb: " Alexa Arenas", twitter: " aalexa arenas", ig: " aal_18", imagen: "../assets/doctor.png"},
     {nombre: " Leonardo Briseño", tel: " +52 1 (33) 1362 0576",correo: " leo_hardy2000@hotmail.com", fb: " Leo Briseño", twitter: " leobriseno", ig: " leonardobriseno", imagen: "../assets/boss.png"},
-    {nombre: " Fernanda Villa ", tel: " +52 1 (33) 1699 8605",correo: " fervillatamez@gmail.com", fb: " Fer Villa", twitter: " fer_villa", ig: " villa_villa", imagen: "../assets/fer.png"},
+    {nombre: " Fernanda Villa ", tel: " +52 1 (33) 1699 8605",correo: " fervillatamez@gmail.com", fb: " Fer Villa", twitter: " fer_villa", ig: " villa_villa0.", imagen: "../assets/fer.png"},
 
   ];
   constructor(public navCtrl: NavController) {
